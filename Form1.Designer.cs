9c796e625ce71e82e2c4fd0834414a151fad8181
Xavier Raemy <bidon@xraemy.ch>
2022-01-23 16:56:05 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace DominoSpy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.slot2 = new DominoSpy.Domino();
+            this.slot3 = new DominoSpy.Domino();
+            this.slot4 = new DominoSpy.Domino();
+            this.slot5 = new DominoSpy.Domino();
+            this.slot6 = new DominoSpy.Domino();
+            this.slot7 = new DominoSpy.Domino();
+            this.slot8 = new DominoSpy.Domino();
+            this.slot9 = new DominoSpy.Domino();
+            this.slot1 = new DominoSpy.Domino();
+            this.slot0 = new DominoSpy.Domino();
+            this.domino8 = new DominoSpy.Domino();
+            this.domino1_out = new DominoSpy.Domino();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(453, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 737);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // slot2
+            // 
+            this.slot2.CorrectA = "";
+            this.slot2.CorrectB = "";
+            this.slot2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot2.Location = new System.Drawing.Point(997, 425);
+            this.slot2.Name = "slot2";
+            this.slot2.ShowCorrect = false;
+            this.slot2.Size = new System.Drawing.Size(100, 200);
+            this.slot2.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot2.TabIndex = 19;
+            this.slot2.TabStop = false;
+            this.slot2.TextA = "0";
+            this.slot2.TextB = "5";
+            this.slot2.Vertical = true;
+            // 
+            // slot3
+            // 
+            this.slot3.CorrectA = "";
+            this.slot3.CorrectB = "";
+            this.slot3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot3.Location = new System.Drawing.Point(1002, 315);
+            this.slot3.MaximumSize = new System.Drawing.Size(200, 100);
+            this.slot3.MinimumSize = new System.Drawing.Size(200, 100);
+            this.slot3.Name = "slot3";
+            this.slot3.ShowCorrect = false;
+            this.slot3.Size = new System.Drawing.Size(200, 100);
+            this.slot3.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot3.TabIndex = 18;
+            this.slot3.TabStop = false;
+            this.slot3.TextA = "0";
+            this.slot3.TextB = "3";
+            this.slot3.Vertical = false;
+            // 
+            // slot4
+            // 
+            this.slot4.CorrectA = "";
+            this.slot4.CorrectB = "";
+            this.slot4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot4.Location = new System.Drawing.Point(1103, 106);
+            this.slot4.Name = "slot4";
+            this.slot4.ShowCorrect = false;
+            this.slot4.Size = new System.Drawing.Size(100, 200);
+            this.slot4.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot4.TabIndex = 17;
+            this.slot4.TabStop = false;
+            this.slot4.TextA = "2";
+            this.slot4.TextB = "3";
+            this.slot4.Vertical = true;
+            // 
+            // slot5
+            // 
+            this.slot5.CorrectA = "";
+            this.slot5.CorrectB = "";
+            this.slot5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot5.Location = new System.Drawing.Point(893, 98);
+            this.slot5.MaximumSize = new System.Drawing.Size(200, 100);
+            this.slot5.MinimumSize = new System.Drawing.Size(200, 100);
+            this.slot5.Name = "slot5";
+            this.slot5.ShowCorrect = false;
+            this.slot5.Size = new System.Drawing.Size(200, 100);
+            this.slot5.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot5.TabIndex = 16;
+            this.slot5.TabStop = false;
+            this.slot5.TextA = "6";
+            this.slot5.TextB = "2";
+            this.slot5.Vertical = false;
+            // 
+            // slot6
+            // 
+            this.slot6.CorrectA = "";
+            this.slot6.CorrectB = "";
+            this.slot6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot6.Location = new System.Drawing.Point(774, 49);
+            this.slot6.Name = "slot6";
+            this.slot6.ShowCorrect = false;
+            this.slot6.Size = new System.Drawing.Size(100, 200);
+            this.slot6.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot6.TabIndex = 15;
+            this.slot6.TabStop = false;
+            this.slot6.TextA = "6";
+            this.slot6.TextB = "6";
+            this.slot6.Vertical = true;
+            // 
+            // slot7
+            // 
+            this.slot7.CorrectA = "";
+            this.slot7.CorrectB = "";
+            this.slot7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot7.Location = new System.Drawing.Point(565, 98);
+            this.slot7.Name = "slot7";
+            this.slot7.ShowCorrect = false;
+            this.slot7.Size = new System.Drawing.Size(200, 100);
+            this.slot7.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot7.TabIndex = 14;
+            this.slot7.TabStop = false;
+            this.slot7.TextA = "5";
+            this.slot7.TextB = "6";
+            this.slot7.Vertical = false;
+            // 
+            // slot8
+            // 
+            this.slot8.CorrectA = "";
+            this.slot8.CorrectB = "";
+            this.slot8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot8.Location = new System.Drawing.Point(556, 206);
+            this.slot8.Name = "slot8";
+            this.slot8.ShowCorrect = false;
+            this.slot8.Size = new System.Drawing.Size(100, 200);
+            this.slot8.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot8.TabIndex = 13;
+            this.slot8.TabStop = false;
+            this.slot8.TextA = "5";
+            this.slot8.TextB = "1";
+            this.slot8.Vertical = true;
+            // 
+            // slot9
+            // 
+            this.slot9.CorrectA = "1";
+            this.slot9.CorrectB = "1";
+            this.slot9.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot9.Location = new System.Drawing.Point(506, 425);
+            this.slot9.MaximumSize = new System.Drawing.Size(200, 100);
+            this.slot9.MinimumSize = new System.Drawing.Size(200, 100);
+            this.slot9.Name = "slot9";
+            this.slot9.ShowCorrect = true;
+            this.slot9.Size = new System.Drawing.Size(200, 100);
+            this.slot9.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot9.TabIndex = 12;
+            this.slot9.TabStop = false;
+            this.slot9.TextA = "1";
+            this.slot9.TextB = "1";
+            this.slot9.Vertical = false;
+            // 
+            // slot1
+            // 
+            this.slot1.CorrectA = "0";
+            this.slot1.CorrectB = "4";
+            this.slot1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot1.Location = new System.Drawing.Point(674, 637);
+            this.slot1.MaximumSize = new System.Drawing.Size(200, 100);
+            this.slot1.MinimumSize = new System.Drawing.Size(200, 100);
+            this.slot1.Name = "slot1";
+            this.slot1.ShowCorrect = true;
+            this.slot1.Size = new System.Drawing.Size(200, 100);
+            this.slot1.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot1.TabIndex = 11;
+            this.slot1.TabStop = false;
+            this.slot1.TextA = "0";
+            this.slot1.TextB = "4";
+            this.slot1.Vertical = false;
+            // 
+            // slot0
+            // 
+            this.slot0.CorrectA = "1";
+            this.slot0.CorrectB = "0";
+            this.slot0.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.slot0.Location = new System.Drawing.Point(556, 537);
+            this.slot0.Name = "slot0";
+            this.slot0.ShowCorrect = true;
+            this.slot0.Size = new System.Drawing.Size(100, 200);
+            this.slot0.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.slot0.TabIndex = 10;
+            this.slot0.TabStop = false;
+            this.slot0.Text = "domino0_out";
+            this.slot0.TextA = "1";
+            this.slot0.TextB = "0";
+            this.slot0.Vertical = true;
+            // 
+            // domino8
+            // 
+            this.domino8.CorrectA = "";
+            this.domino8.CorrectB = "";
+            this.domino8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.domino8.Location = new System.Drawing.Point(230, 652);
+            this.domino8.MaximumSize = new System.Drawing.Size(200, 100);
+            this.domino8.MinimumSize = new System.Drawing.Size(200, 100);
+            this.domino8.Name = "domino8";
+            this.domino8.ShowCorrect = false;
+            this.domino8.Size = new System.Drawing.Size(200, 100);
+            this.domino8.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.domino8.TabIndex = 9;
+            this.domino8.TabStop = false;
+            this.domino8.Text = "domino8";
+            this.domino8.TextA = "3";
+            this.domino8.TextB = "2";
+            this.domino8.Vertical = false;
+            // 
+            // domino1_out
+            // 
+            this.domino1_out.CorrectA = "";
+            this.domino1_out.CorrectB = "";
+            this.domino1_out.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.domino1_out.Location = new System.Drawing.Point(12, 652);
+            this.domino1_out.MaximumSize = new System.Drawing.Size(200, 100);
+            this.domino1_out.MinimumSize = new System.Drawing.Size(200, 100);
+            this.domino1_out.Name = "domino1_out";
+            this.domino1_out.ShowCorrect = false;
+            this.domino1_out.Size = new System.Drawing.Size(200, 100);
+            this.domino1_out.Status = DominoSpy.DominoStatus.NotPlaced;
+            this.domino1_out.TabIndex = 3;
+            this.domino1_out.TabStop = false;
+            this.domino1_out.TextA = "0";
+            this.domino1_out.TextB = "4";
+            this.domino1_out.Vertical = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 784);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot4);
+            this.Controls.Add(this.slot5);
+            this.Controls.Add(this.slot6);
+            this.Controls.Add(this.slot7);
+            this.Controls.Add(this.slot8);
+            this.Controls.Add(this.slot9);
+            this.Controls.Add(this.slot1);
+            this.Controls.Add(this.slot0);
+            this.Controls.Add(this.domino8);
+            this.Controls.Add(this.domino1_out);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(1273, 823);
+            this.MinimumSize = new System.Drawing.Size(1273, 823);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Domino domino1_out;
+        private Domino domino8;
+        private Domino slot0;
+        private Domino slot1;
+        private Domino slot9;
+        private Domino slot8;
+        private Domino slot7;
+        private Domino slot6;
+        private Domino slot5;
+        private Domino slot4;
+        private Domino slot3;
+        private Domino slot2;
+    }
+}
+
---
+++ domino.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace DominoSpy
+{ 
+
+    public enum DominoStatus
+    {
+        Correct,
+        Wrong,
+        Inverted,
+        NotPlaced
+    }
+
+    public partial class Domino : Control
+    {
+        private readonly Color _colorOutline = Color.Black;
+        private readonly Color _colorVuPath = Color.DarkGreen;
+        private readonly Color _colorVuFill = Color.Green;
+        private readonly Color _colorArrowPath = Color.DarkRed;
+        private readonly Color _colorArrowFill = Color.Red;
+        private readonly Color _colorCrossPath = Color.DarkRed;
+        private readonly Color _colorCrossFill = Color.Red;
+        private readonly Color _colorText = Color.Black;
+        private readonly Color _colorCorrectText = Color.Green;
+        private readonly int _smallDim = 100;
+        private readonly int _largeDim = 200;
+        private readonly int _fontSize = 50;
+        private readonly int _fontCorrect = 30;
+        private String _textA = "";
+        private String _textB = "";
+        private String _correctA = "";
+        private String _correctB = "";
+        private bool _showCorrect = false;
+        private bool _vert = false;
+        private DominoStatus _status = DominoStatus.NotPlaced;
+
+
+        public bool Vertical
+        {
+            get { return _vert; }
+            set
+            {
+                _vert = value;
+                if (_vert)
+                {
+                    this.Size = new Size(_smallDim, _largeDim);
+                }
+                else
+                {
+                    this.Size = new Size(_largeDim, _smallDim);
+                }
+                Invalidate(); 
+            }
+        }
+
+        public DominoStatus Status
+        {
+            get { return _status; }
+            set { _status = value; Invalidate(); }
+        }
+
+        public String TextA
+        {
+            get { return _textA; }
+            set { _textA = value; Invalidate(); }
+        }
+        public String TextB
+        {
+            get { return _textB; }
+            set { _textB = value; Invalidate(); }
+        }
+
+        public String CorrectA
+        {
+            get { return _correctA; }
+            set { _correctA = value; Invalidate(); }
+        }
+        public String CorrectB
+        {
+            get { return _correctB; }
+            set { _correctB = value; Invalidate(); }
+        }
+
+        public bool ShowCorrect
+        {
+            get { return _showCorrect; }
+            set { _showCorrect = value; Invalidate(); }
+        }
+
+
+        public Domino()
+        {
+            InitializeComponent();
+            
+            if (_vert)
+            {
+                this.Size = new Size(_smallDim, _largeDim);
+            }
+            else
+            {
+                this.Size = new Size(_largeDim, _smallDim);
+            }
+
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+
+            DrawBody(pe);
+            DrawTexts(pe);
+            if (_showCorrect)
+            {
+                DrawCorrect(pe);
+            }
+
+            switch (_status)
+            {
+                case DominoStatus.Correct:
+                    DrawVu(pe);
+                    break;
+                case DominoStatus.Wrong:
+                    DrawCross(pe);
+                    break;
+
+                case DominoStatus.Inverted:
+                    DrawArrow(pe);
+                    break;
+            }
+            base.OnPaint(pe);
+        }
+
+        protected void DrawBody(PaintEventArgs pe)
+        {
+            List<PointF> shapePoints = new List<PointF> {
+                new PointF(- _largeDim/2, _smallDim/2),
+                new PointF(- _largeDim/2, -_smallDim/2),
+                new PointF(_largeDim/2, -_smallDim/2),
+                new PointF( _largeDim/2, _smallDim/2)
+            };
+
+            List<PointF> drawPoints = new List<PointF>();
+            if (_vert)
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_smallDim / 2 + p.Y, _largeDim / 2 + p.X));
+                }
+            }
+            else
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_largeDim / 2 + p.X, _smallDim / 2 + p.Y));
+                }
+            }
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddLines(drawPoints.ToArray());
+            path.CloseFigure();
+
+            pe.Graphics.FillPath(new SolidBrush(this.ForeColor), path);
+            pe.Graphics.DrawPath(new Pen(_colorOutline, 2), path);
+            path.Dispose();
+        }
+
+        protected void DrawVu(PaintEventArgs pe)
+        {
+
+            List<PointF> shapePoints = new List<PointF> {
+                new PointF(-27, 4),
+                new PointF(-11, 27),
+                new PointF(24, -26),
+                new PointF(20, -28),
+                new PointF(-11, 10),
+                new PointF(-25, 1 )
+            };
+
+            List<PointF> drawPoints = new List<PointF>();
+            if (_vert)
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_smallDim / 2 + p.Y, _largeDim / 2 + p.X));
+                }
+            }
+            else
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_largeDim / 2 + p.X, _smallDim / 2 + p.Y));
+                }
+            }
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddLines(drawPoints.ToArray());
+            path.CloseFigure();
+
+            pe.Graphics.FillPath(new SolidBrush(_colorVuFill), path);
+            pe.Graphics.DrawPath(new Pen(_colorVuPath, 2), path);
+            path.Dispose();
+        }
+
+        protected void DrawArrow(PaintEventArgs pe)
+        {
+
+            List<PointF> shapePoints = new List<PointF> {
+                new PointF(-3 , -3),
+                new PointF(-27, -3),
+                new PointF(-11, -20),
+                new PointF(-21, -20),
+                new PointF(-41, +0),
+                new PointF(-21, +20),
+                new PointF(-11, +20),
+                new PointF(-27, +3),
+                new PointF(-3 , +3)
+            };
+
+            List<PointF> drawPointsA = new List<PointF>();
+            List<PointF> drawPointsB = new List<PointF>();
+
+            if (_vert)
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPointsA.Add(new PointF(_smallDim / 2 + p.Y, _largeDim / 2 + p.X));
+                    drawPointsB.Add(new PointF(_smallDim / 2 + p.Y, _largeDim / 2 - p.X));
+                }
+            } 
+            else
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPointsA.Add(new PointF(_largeDim / 2 + p.X, _smallDim / 2 + p.Y));
+                    drawPointsB.Add(new PointF(_largeDim / 2 - p.X, _smallDim / 2 + p.Y));
+                }
+            }
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddLines(drawPointsA.ToArray());
+            path.CloseFigure();
+
+            path.StartFigure();
+            path.AddLines(drawPointsB.ToArray());
+            path.CloseFigure();
+
+            pe.Graphics.FillPath(new SolidBrush(_colorArrowFill), path);
+            pe.Graphics.DrawPath(new Pen(_colorArrowPath, 2), path);
+            path.Dispose();
+        }
+
+        protected void DrawCross(PaintEventArgs pe)
+        {
+
+            List<PointF> shapePoints = new List<PointF> {
+                new PointF(0 , -5),
+                new PointF(-13, -17),
+                new PointF(-18, -12)
+            };
+
+
+            shapePoints.Add(new PointF(shapePoints[0].Y, shapePoints[0].X));
+            shapePoints.Add(new PointF(shapePoints[2].X, -shapePoints[2].Y));
+            shapePoints.Add(new PointF(shapePoints[1].X, -shapePoints[1].Y));
+            shapePoints.Add(new PointF(shapePoints[0].X, -shapePoints[0].Y));
+            shapePoints.Add(new PointF(-shapePoints[1].X, -shapePoints[1].Y));
+            shapePoints.Add(new PointF(-shapePoints[2].X, -shapePoints[2].Y));
+            shapePoints.Add(new PointF(-shapePoints[0].Y, shapePoints[0].X));
+            shapePoints.Add(new PointF(-shapePoints[2].X, shapePoints[2].Y));
+            shapePoints.Add(new PointF(-shapePoints[1].X, shapePoints[1].Y));
+
+            List<PointF> drawPoints = new List<PointF>();
+            if (_vert)
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_smallDim / 2 + p.X, _largeDim / 2 + p.Y));
+                }
+            } else
+            {
+                foreach (PointF p in shapePoints)
+                {
+                    drawPoints.Add(new PointF(_largeDim / 2 + p.X, _smallDim / 2 + p.Y));
+                }
+            }
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddLines(drawPoints.ToArray());
+            path.CloseFigure();
+
+            pe.Graphics.FillPath(new SolidBrush(_colorCrossFill), path);
+            pe.Graphics.DrawPath(new Pen(_colorCrossPath, 2), path);
+            path.Dispose();
+        }
+
+        void DrawTexts(PaintEventArgs pe)
+        {
+            
+            Point positionA; 
+            Point positionB;
+            if (_vert)
+            {
+                positionA = new Point(_smallDim / 2, _largeDim / 2 - _largeDim / 4);
+                positionB = new Point(_smallDim / 2, _largeDim / 2 + _largeDim / 4);
+            }
+            else
+            {
+                positionA = new Point(_largeDim / 2 - _largeDim /4 , _smallDim / 2);
+                positionB = new Point(_largeDim / 2 + _largeDim /4, _smallDim / 2);
+            }
+
+            DrawTextAt(pe, positionA, TextA, _colorText, _fontSize);
+            DrawTextAt(pe, positionB, TextB, _colorText, _fontSize);
+
+        }
+
+        void DrawCorrect(PaintEventArgs pe)
+        {
+
+            Point positionA;
+            Point positionB;
+            if (_vert)
+            {
+                positionA = new Point(_smallDim*2 / 10, _largeDim / 2 - _largeDim / 4);
+                positionB = new Point(_smallDim*2 / 10, _largeDim / 2 + _largeDim / 4);
+            }
+            else
+            {
+                positionA = new Point(_largeDim / 2 - _largeDim / 4, _smallDim*8 / 10);
+                positionB = new Point(_largeDim / 2 + _largeDim / 4, _smallDim*8 / 10);
+            }
+
+            DrawTextAt(pe, positionA, _correctA, _colorCorrectText, _fontCorrect);
+            DrawTextAt(pe, positionB, _correctB, _colorCorrectText, _fontCorrect);
+
+        }
+
+
+        void DrawTextAt(PaintEventArgs pe, Point pos, String text, Color col, int size)
+        {
+            FontFamily fontFamily = new FontFamily("Arial");
+            Font font = new Font(
+               fontFamily,
+               size,
+               FontStyle.Regular,
+               GraphicsUnit.Pixel);
+            SolidBrush solidBrush = new SolidBrush(col);
+
+            Rectangle rect1 = new Rectangle(pos.X - _fontSize/2 , pos.Y - _fontSize/2 + _fontSize / 12, _fontSize, _fontSize);
+
+            StringFormat stringFormat = new StringFormat
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            pe.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            pe.Graphics.DrawString(text, font, solidBrush, rect1, stringFormat);
+
+            stringFormat.Dispose();
+            fontFamily.Dispose();
+            font.Dispose();
+        }
+    }
+}
